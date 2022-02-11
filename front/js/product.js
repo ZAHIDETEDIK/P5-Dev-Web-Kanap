cbdf4eaf11d74f833955a3e0dc0dc6f32b10a31e
Tedik <tamureylem@gmail.com>
2022-02-11 22:20:28 +0100

    localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -21,25 +21,56 @@ fetch(`http://localhost:3000/api/products/${idProduct}`)
   
   let productPrice=document.getElementById("price")
   productPrice.innerHTML=product.price
-  
+
   for(let colors of product.colors){
   console.table(colors)
   let productColors=document.createElement("option")
   document.querySelector("#colors").appendChild(productColors)
   productColors.value=colors
   productColors.innerHTML=colors
-  
-
-
-}
+     }
+ 
 })
 }
+const button=document.querySelector("#addToCart")
+{ 
+button. addEventListener("click", (e) =>{
+const color = document.querySelector("#colors").value
+const quantity = document.querySelector("#quantity").value
+if (color==null ||color==""|| quantity==null||quantity==0)
+{ alert ("svp choisissez une couleur et une quantité")
+return
+} 
+  const imageUrl=document.querySelector("#img")
+  let price=document.querySelector("#price").textContent
+//options des articles
+const option = { 
+  id:idProduct,
+  couleur:color,
+  quantity:Number(quantity),
+  price:price,
+  img:imageUrl,
+  
+ 
+
+}
+localStorage.setItem(idProduct,JSON.stringify(option))
+window.location.href="cart.html"
+
+})
+}
+
+
+
+
+
+
+
+
+
   
 
-
-
-
-
-
-
+ 
+  
+    
 
