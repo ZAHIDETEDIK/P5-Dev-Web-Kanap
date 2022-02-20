522eeb16b5598321300ad229e5d2b543ba7f77af
Tedik <tamureylem@gmail.com>
2022-02-20 22:35:03 +0100

    article /image

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,6 @@
 const cart = []
 recupItem()
 
-cart.forEach((item) => displayItem(item))
-
-const orderButton = document.querySelector("#order")
-orderButton.addEventListener("click", (e) => submitForm(e))
 
 
 // Récupération des données du localstorage
@@ -18,44 +14,34 @@ function recupItem() {
 }
 // Affichage des éléments du Panier
 function displayItem(item) {
-    const article = makeArticle(item)
-    const imageDiv = makeImageDiv(item)
-    article.appendChild(imageDiv)
 
-    const cardItemContent = makeCartContent(item)
-    article.appendChild(cardItemContent)
-    displayArticle(article)
-    displayTotalQuantity()
-    displayTotalPrice()
+    const article=makeArticle(item)
+    displayArticle
+    const div = makeImageDiv(item)
+    article.appendChild(div)
 }
-// Création de la div "cart__item__content"
-function makeCartContent(item) {
-    const cardItemContent = document.createElement("div")
-    cardItemContent.classList.add("cart__item__content")
+    function displayArticle(article){
 
-    const description = makeDescription(item)
-    const settings = makeSettings(item)
+        document.querySelector("#cart__items").appendChild(article)
 
-    cardItemContent.appendChild(description)
-    cardItemContent.appendChild(settings)
-    return cardItemContent
-}
-// Insertion de "description"
-function makeDescription(item) {
-    const description = document.createElement("div")
-    description.classList.add("cart__item__content__description")
+    }
 
-    const h2 = document.createElement("h2")
-    h2.textContent = item.name
+    function makeArticle(item){
 
-    const p = document.createElement("p")
-    p.textContent = item.color;
+        const article=document.createElement("article")
+        article. classlist .add ("card__item")
+        article.dataset.id=item.id
+        article.dataset.color=item.color
+    }
 
-    const p2 = document.createElement("p")
-    p2.textContent = item.price + "€"
+function makeImageDiv(item){
 
-    description.appendChild(h2)
-    description.appendChild(p)
-    description.appendChild(p2)
-    return description
+         const div=document.createElement("div")
+         div .classlist.add("cart__item__img")
+         const image= document.createElement("img")
+         image.src=item.imageUrl
+         image.alt=item.altTxt
+         div.appendChild(image)
+        return div
+
 }
